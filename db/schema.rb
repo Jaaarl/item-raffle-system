77f54a2405fdfde8c04a4380365a63df43c701e5
jaaarl <jarl13.work@gmail.com>
2024-10-25 23:44:21 +0800

    modify: change phone from Integer to String [Feature 1]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_24_073228) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_25_145533) do
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -20,7 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_24_073228) do
     t.string "username"
     t.integer "role", default: 0, null: false
     t.integer "coins"
-    t.integer "phone"
+    t.string "phone"
     t.float "total_deposit"
     t.integer "children_members"
     t.datetime "created_at", null: false
