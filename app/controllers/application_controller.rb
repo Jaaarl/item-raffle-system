a47f493ab54fc9f636f3301d0e9da927384c4545
jaaarl <jarl13.work@gmail.com>
2024-10-28 02:09:33 +0800

    modify: sign in redirect to role index [Feature 1]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  def after_sign_up_path_for(resource)
+    after_sign_in_path_for(resource) if is_navigational_format?
+  end
+
+  def after_sign_in_path_for(resource)
+    if current_user.admin?
+      admins_path
+    else
+      clients_path
+    end
+  end
 end
