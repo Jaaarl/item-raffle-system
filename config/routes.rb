d0f31ea06a3beb108b40f47c06652b10549a44a8
jaaarl <jarl13.work@gmail.com>
2024-10-29 06:11:12 +0800

    modify: Admin users now have a dedicated login page [Feature 1]

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+  devise_scope :user do
+    get 'admin/login', to: 'admin/sessions#new', as: :new_admin_session
+    post 'admin/login', to: 'admin/sessions#create', as: :admin_session
+    delete 'admin/logout', to: 'admin/sessions#destroy', as: :destroy_admin_session
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
