4d7c93aa02458930a5e5df591b73695eee83d942
Jaaarl <jarl13.work@gmail.com>
2024-10-29 14:06:57 +0800

    modify: constrain the new routes [Feature 2]

--- config/routes.rb
+++ config/routes.rb
@@ -3,20 +3,7 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-  devise_scope :user do
-    get 'admin/login', to: 'admin/sessions#new', as: :new_admin_session
-    post 'admin/login', to: 'admin/sessions#create', as: :admin_session
-    delete 'admin/logout', to: 'admin/sessions#destroy', as: :destroy_admin_session
-  end
 
-  devise_scope :user do
-    get 'client/login', to: 'client/sessions#new', as: :new_client_session
-    post 'client/login', to: 'client/sessions#create', as: :client_session
-    delete 'client/logout', to: 'client/sessions#destroy', as: :destroy_client_session
-
-    get 'client/register', to: 'client/registrations#new', as: :new_client_registration
-    post 'client/register', to: 'client/registrations#create', as: :client_registration
-  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -24,8 +11,23 @@ Rails.application.routes.draw do
   root 'home#index'
   constraints(ClientDomainConstraint.new) do
     resources :clients, only: [:index ]
+
+    devise_scope :user do
+      get 'client/login', to: 'client/sessions#new', as: :new_client_session
+      post 'client/login', to: 'client/sessions#create', as: :client_session
+      delete 'client/logout', to: 'client/sessions#destroy', as: :destroy_client_session
+
+      get 'client/register', to: 'client/registrations#new', as: :new_client_registration
+      post 'client/register', to: 'client/registrations#create', as: :client_registration
+    end
   end
   constraints(AdminDomainConstraint.new) do
     resources :admins, only: [:index]
+
+    devise_scope :user do
+      get 'admin/login', to: 'admin/sessions#new', as: :new_admin_session
+      post 'admin/login', to: 'admin/sessions#create', as: :admin_session
+      delete 'admin/logout', to: 'admin/sessions#destroy', as: :destroy_admin_session
+    end
   end
 end
