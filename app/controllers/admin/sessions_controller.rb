be88faac1b41be20ff72e2f59e29cd4b24702de6
Jaaarl <jarl13.work@gmail.com>
2024-10-30 21:20:08 +0800

    modify: update routes & path for admin

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -10,4 +10,11 @@ class Admin::SessionsController < Devise::SessionsController
   def destroy
     super
   end
+
+  def after_sign_in_path_for(resource)
+    admins_path
+  end
+  def after_sign_out_path_for(resource)
+    new_admin_user_session_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   root 'home#index'
   constraints(ClientDomainConstraint.new) do
     resources :clients, only: [:index ]
-
     devise_scope :user do
       get 'client/login', to: 'client/sessions#new', as: :new_client_session
       post 'client/login', to: 'client/sessions#create', as: :client_session
@@ -23,11 +13,8 @@ Rails.application.routes.draw do
   end
   constraints(AdminDomainConstraint.new) do
     resources :admins, only: [:index]
-
-    devise_scope :user do
-      get 'admin/login', to: 'admin/sessions#new', as: :new_admin_session
-      post 'admin/login', to: 'admin/sessions#create', as: :admin_session
-      delete 'admin/logout', to: 'admin/sessions#destroy', as: :destroy_admin_session
-    end
+    devise_for :users, controllers: {
+      sessions: 'admin/sessions',
+    }, as: :admin
   end
 end
