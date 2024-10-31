3db325912558c27f09d9f71ae0c4053762c0c096
Jaaarl <jarl13.work@gmail.com>
2024-10-31 11:50:10 +0800

    modify: Implement Bootstrap for Client Register design [Feature 3]

--- app/controllers/client/registrations_controller.rb
+++ app/controllers/client/registrations_controller.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 class Client::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
   def after_sign_in_path_for(resource)
     clients_path
+  end
+
+  protected
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
 end
