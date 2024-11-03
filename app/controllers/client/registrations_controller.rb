6012f992c48ea45b2917b2b70704466da4886a0b
jaaarl <jarl13.work@gmail.com>
2024-11-04 04:31:28 +0800

    add: Edit user profile functionality [Feature 4]

--- app/controllers/client/registrations_controller.rb
+++ app/controllers/client/registrations_controller.rb
@@ -2,12 +2,37 @@
 
 class Client::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
+
   def after_sign_in_path_for(resource)
     client_homepage_index_path
   end
 
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+    resource_updated = update_resource(resource, account_update_params)
+    yield resource if block_given?
+    if resource_updated
+      set_flash_message_for_update(resource, prev_unconfirmed_email)
+      bypass_sign_in resource, scope: resource_name if sign_in_after_change_password?
+
+      redirect_to client_homepage_index_path
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      flash[:alert] = 'There was a problem editing your profile. Please check your fields and try again.'
+      redirect_to edit_client_user_registration_path
+    end
+  end
+  
   protected
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:phone, :username])
   end
 end
