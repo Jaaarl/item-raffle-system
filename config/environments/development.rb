78ba69d08904f17811999f0b45afa891a54b7eaa
Jaaarl <jarl13.work@gmail.com>
2024-11-06 11:04:09 +0800

    add: enable asset debugging for StimulusJS in development environment [Feature 6]

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.assets.debug = true
   config.hosts << "client.com"
   config.hosts << "admin.com"
 end
