cba6818eb69e3f54b15bb6e0d4b6d5de70deac30
Jaaarl <jarl13.work@gmail.com>
2024-11-07 16:03:27 +0800

    refactor: reset schema, drop addresses

--- db/schema.rb
+++ db/schema.rb
@@ -45,23 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_07_024157) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "addresses", charset: "utf8mb4", force: :cascade do |t|
-    t.string "genre", default: "0", null: false
-    t.string "name"
-    t.string "street_address"
-    t.string "phone_number"
-    t.text "remark"
-    t.boolean "is_default", default: false
-    t.bigint "user_id", null: false
-    t.bigint "region_id", null: false
-    t.bigint "province_id", null: false
-    t.bigint "city_id", null: false
-    t.bigint "barangay_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "fk_rails_48c9e0c5a2"
-  end
-
   create_table "places", charset: "utf8mb4", force: :cascade do |t|
     t.integer "genre", default: 0, null: false
     t.string "name"
@@ -102,7 +85,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_07_024157) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "addresses", "users"
   add_foreign_key "places", "address_barangays"
   add_foreign_key "places", "address_cities"
   add_foreign_key "places", "address_provinces"
