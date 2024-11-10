7d1735d6ec3e02eeb7b2fcf3db034639ad959847
jaaarl <jarl13.work@gmail.com>
2024-11-11 00:55:01 +0800

    Fix: Prevent non-client users from logging in as client

--- app/controllers/client/sessions_controller.rb
+++ app/controllers/client/sessions_controller.rb
@@ -1,4 +1,19 @@
 class Client::SessionsController < Devise::SessionsController
+  def create
+    self.resource = warden.authenticate!(auth_options)
+
+    if resource.client?
+      set_flash_message!(:notice, :signed_in)
+      sign_in(resource_name, resource)
+      yield resource if block_given?
+      respond_with resource, location: after_sign_in_path_for(resource)
+    else
+      sign_out(resource_name)
+      set_flash_message!(:alert, :not_client)
+      redirect_to new_session_path(resource_name)
+    end
+  end
+  
   def after_sign_in_path_for(resource)
     client_homepage_index_path
   end
