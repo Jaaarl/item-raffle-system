ba608e9e17d29adce130ede3441404f63e2dd17a
Jaaarl <jarl13.work@gmail.com>
2024-11-11 18:08:17 +0800

    modify: user have a parent and child relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :places
+  has_many :children, class_name: 'User', foreign_key: 'parent_id', dependent: :nullify
+  belongs_to :parent, class_name: 'User', optional: true
   enum role: { client: 0, admin: 1 }
   mount_uploader :image, ImageUploader
   validates :phone, phone: true, allow_blank: true
