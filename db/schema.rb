67c0d9e267344e678b652785ea9b3b5009d0d880
Jaaarl <jarl13.work@gmail.com>
2024-11-11 17:57:49 +0800

    modify: change parent_id type to int

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_11_025216) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_11_075002) do
   create_table "address_barangays", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "city_id"
     t.string "code"
@@ -96,7 +96,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_11_025216) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
-    t.string "parent_id"
+    t.integer "parent_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
