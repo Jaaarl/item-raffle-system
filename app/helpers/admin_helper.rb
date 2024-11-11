2a92cecef57100a8c89059c27635e6a324ff7954
Jaaarl <jarl13.work@gmail.com>
2024-11-11 19:04:24 +0800

    refactor: simplify parent-child sum calculation

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,10 +1,4 @@
 module AdminHelper
-  def member_total_deposit(email)
-    total_deposit = User.where(parent_id: email)
-    total_deposit = total_deposit.map(&:total_deposit).compact
-    total_deposit.sum
-  end
-
   def total_used_coins(deposit, coins)
     deposit ||= 0
     coins ||= 0
