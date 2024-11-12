d23f0f65e3545f08be67dad61474c6962f45a04a
Jaaarl <jarl13.work@gmail.com>
2024-11-12 12:14:50 +0800

    Add: Implement crud for item [Feature 10]

--- app/controllers/admin/item_management_controller.rb
+++ app/controllers/admin/item_management_controller.rb
@@ -1,4 +1,59 @@
 class Admin::ItemManagementController < Admin::BaseController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
+    @items = Item.all
+  end
+
+  def show
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_management_index_path, notice: 'Item was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to admin_item_management_index_path, notice: 'Item was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to admin_item_management_index_path, notice: 'Item was successfully deleted.'
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(
+      :name,
+      :quantity,
+      :minimum_tickets,
+      :state,
+      :batch_count,
+      :online_at,
+      :offline_at,
+      :start_at,
+      :status,
+      :image
+    )
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
   enum status: { active: 0, inactive: 1}
+
+  mount_uploader :image, ImageUploader
 end
