7dc05022e64e20d8aea74ecc160fb632593a47e8
jaaarl <jarl13.work@gmail.com>
2024-11-13 03:27:01 +0800

    add: Prevent deletion of category if associated with item [Feature 12]

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,6 @@
 class Category < ApplicationRecord
-  has_many :items, dependent: :restrict_with_error
   validates :name, presence: true
 
-  has_many :item_category_ships
+  has_many :item_category_ships, dependent: :restrict_with_error
   has_many :items, through: :item_category_ships
 end
