ecdb6c6c6bfb4d9e2e01394408b7fc65ac8d1646
Jaaarl <jarl13.work@gmail.com>
2024-11-18 10:07:56 +0800

    refactor: include categories to eliminate n+1

--- app/controllers/client/lottery_controller.rb
+++ app/controllers/client/lottery_controller.rb
@@ -1,6 +1,6 @@
 class Client::LotteryController < ApplicationController
   def index
-    @items = Item.where(state: 'starting', status: 'active')
+    @items = Item.includes(:categories).where(state: 'starting', status: 'active')
     if params[:category_id].present?
       @items = @items.joins(:categories).where(categories: { id: params[:category_id] })
     end
