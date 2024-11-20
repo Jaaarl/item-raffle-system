4e3e329ae1609a5422c505508c5a5dd7bec76f84
Jaaarl <jarl13.work@gmail.com>
2024-11-20 20:14:33 +0800

    add: enum for order genre

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,8 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :offer
 
+  enum genre: { deposit: 0, increase: 1, deduct: 2, bonus: 3, share: 4 }
+
   aasm column: :state do
     state :pending, initial: true
     state :submitted, :paid, :cancelled
