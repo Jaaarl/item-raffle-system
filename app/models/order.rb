3a9d304998a4af22ff0b42e3a082197152efa31b
Jaaarl <jarl13.work@gmail.com>
2024-11-20 22:47:51 +0800

    modify: create method for submitted to paid [Feature 20]

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,8 @@
 class Order < ApplicationRecord
   include AASM
 
+  after_create :assign_serial_number
+
   belongs_to :user
   belongs_to :offer
 
@@ -19,7 +21,41 @@ class Order < ApplicationRecord
     end
 
     event :pay do
-      transitions from: :submitted, to: :paid
+      transitions from: :submitted, to: :paid,
+                  success: :update_value_post_paid
     end
+  end
+
+  private
+  def update_value_post_paid
+    if self.deduct?
+      deduct_coins
+    elsif self.deposit?
+      increase_total_deposit
+    else
+      add_coins
+    end
+  end
+
+  def add_coins
+    user.coins = user.coins + offer.coin
+    user.save
+  end
+
+  def deduct_coins
+    user.coins -= offer.coin
+    user.save
+  end
+
+  def increase_total_deposit
+    user.total += offer.amount
+    user.save
+  end
+
+  def assign_serial_number
+    number_count = Order.where(offer_id: offer_id, user_id: user_id).count
+    formatted_number_count = number_count.to_s.rjust(4, '0')
+    time = Time.current.strftime("%y%m%d")
+    self.serial_number = "#{time}-#{offer.id}-#{user_id}-#{formatted_number_count}"
   end
 end
