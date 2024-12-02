ddd8bf70e5f14ded0febe8d9032597efb98dc820
Jaaarl <jarl13.work@gmail.com>
2024-12-02 13:26:10 +0800

    add: winner's feedback in homepage [Feature 33]

--- app/controllers/client/homepage_controller.rb
+++ app/controllers/client/homepage_controller.rb
@@ -2,5 +2,6 @@ class Client::HomepageController < ApplicationController
   def index
     @banners = Banner.active.where('online_at <= ? AND offline_at > ?', Date.current, Date.current)
     @news_tickers = NewsTicker.active
+    @winners_feedback = Winner.includes(:user).published.last(5)
   end
 end
