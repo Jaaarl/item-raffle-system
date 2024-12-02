ae4f7768d5c97c2a3a178837239e1c9f7ffdb7e2
Jaaarl <jarl13.work@gmail.com>
2024-12-02 13:35:52 +0800

    add: lottery items in homepage [Feature 33]

--- app/controllers/client/homepage_controller.rb
+++ app/controllers/client/homepage_controller.rb
@@ -3,5 +3,6 @@ class Client::HomepageController < ApplicationController
     @banners = Banner.active.where('online_at <= ? AND offline_at > ?', Date.current, Date.current)
     @news_tickers = NewsTicker.active
     @winners_feedback = Winner.includes(:user).published.last(5)
+    @items = Item.includes(:categories).starting.active.last(8)
   end
 end
