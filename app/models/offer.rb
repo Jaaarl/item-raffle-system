2cbcf984537c94aa8827b8f6492b495f86e7c5db
Jaaarl <jarl13.work@gmail.com>
2024-12-11 11:40:57 +0800

    add: genre for offers [Feature 40]

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,4 +2,5 @@ class Offer < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   enum status: { inactive: 0, active: 1 }
+  enum genre: { one_time: 1, monthly: 2, weekly: 3, daily: 4, regular: 5 }
 end
