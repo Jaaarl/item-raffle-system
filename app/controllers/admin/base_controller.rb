0c50e1f35c99cc2bb2b3868b8211aff33f510d88
Jaaarl <jarl13.work@gmail.com>
2024-12-15 23:56:42 +0800

    modify: rearrange sidebar content & add drop down for language

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,4 +1,16 @@
 class Admin::BaseController < ActionController::Base
   before_action :authenticate_admin_user!
+  before_action :set_locale
+
   layout 'admin'
+
+  private
+
+  def set_locale
+    if params[:locale] && I18n.available_locales.include?(params[:locale].to_sym)
+      session[:locale] = params[:locale]
+    end
+
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
 end
