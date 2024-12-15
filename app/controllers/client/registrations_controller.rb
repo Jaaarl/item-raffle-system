02d056e14609e734b74dbaf1cce6a00205a87438
Jaaarl <jarl13.work@gmail.com>
2024-12-15 15:46:28 +0800

    modify: tl translation for registration, session and devise translation

--- app/controllers/client/registrations_controller.rb
+++ app/controllers/client/registrations_controller.rb
@@ -62,7 +62,7 @@ class Client::RegistrationsController < Devise::RegistrationsController
     yield resource if block_given?
     if resource_updated
       set_flash_message_for_update(resource, prev_unconfirmed_email)
-      flash[:notice] = 'User profile has been updated successfully.'
+      flash[:notice] = t('flash.notice.user_profile_updated')
       bypass_sign_in resource, scope: resource_name if sign_in_after_change_password?
       redirect_to client_me_index_path
     else
