15e964a8fbc517f1fcda696d7ef62ac6b86111b7
Jaaarl <jarl13.work@gmail.com>
2024-12-16 10:09:06 +0800

    modify: add seed for member level

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+MemberLevel.create!(
+  level: 1,
+  required_members: 0,
+  coins: 0,
+  )
 User.create!(
   email: 'admin@gmail.com',
   password: '123456',
   password_confirmation: '123456',
-  role: 'admin'
+  role: 'admin',
+  member_level: MemberLevel.first
 )
 User.create!(
   email: 'client@gmail.com',
   password: '123456',
-  password_confirmation: '123456'
+  password_confirmation: '123456',
+  member_level: MemberLevel.first
 )
+
